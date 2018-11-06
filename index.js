6d019bc7783fc4c9ae0cdc1cd805d7824855e0f0
ppbra <kaka020998@gmail.com>
2018-11-06 09:41:46 +0700

    ok

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const server = require('http').Server(app);
 
 const io = require('socket.io')(server);
 
-server.listen(process.env.PORT || 3001);
+server.listen(process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3001);
 
 
 io.on('connection',(socket)=>{
@@ -14,6 +14,11 @@ io.on('connection',(socket)=>{
         console.log("da gui tin nhan!");
         socket.broadcast.emit("recieve-message",data);
     })
+})
+
+
+app.get('/', (req, res) => {
+    res.send("Home page. Server running okay.");
 })
 
 
